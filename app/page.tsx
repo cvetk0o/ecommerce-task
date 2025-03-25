814790ba7a8da1085c2476ddb6dbb29e9286d673
cvetk0o <petar.ponjevic@gmail.com>
2025-03-25 14:39:57 +0100

    feature: installed husky

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,10 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
+
+
+
+
   return (
     <div className={styles.page}>
       <main className={styles.main}>
