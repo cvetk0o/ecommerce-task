f4955b80587195f5d45ed1bb251174b2fa9467cf
cvetk0o <petar.ponjevic@gmail.com>
2025-03-25 18:34:40 +0100

    feature: custom favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "E-commerce Task",
   description: "",
+  icons: {
+    icon: "/favicon.svg",
+  },
 };
 
 export default function RootLayout({
