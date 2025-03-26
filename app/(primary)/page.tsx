0ee7bb978132f49bbba24c466e64d19b78be2458
cvetk0o <petar.ponjevic@gmail.com>
2025-03-26 22:37:48 +0100

    feature: dynamic brand name

--- app/(primary)/page.tsx
+++ app/(primary)/page.tsx
@@ -3,12 +3,17 @@ import { MarkedTextElements } from "@/components/MarkedText/types";
 import ProductsSection from "@/components/ProductsSection";
 import { getFeaturedProducts } from "@/services/products";
 
+const BRAND_NAME = process.env.NEXT_BRAND_NAME || "";
+
 export default async function Home() {
   const products = await getFeaturedProducts();
 
   return (
     <>
-      <MarkedText text="Welcome to Ecommerce" element={MarkedTextElements.h1} />
+      <MarkedText
+        text={`Welcome to ${BRAND_NAME}`}
+        element={MarkedTextElements.h1}
+      />
       <ProductsSection sectionTitle="Recommended for you" products={products} />
     </>
   );
