d7da6a52ed5a01b332a30054cc20c328da831eaa
cvetk0o <petar.ponjevic@gmail.com>
2025-03-27 17:06:07 +0100

    feature[style]: price styling

--- components/CartSummary/index.tsx
+++ components/CartSummary/index.tsx
@@ -7,10 +7,14 @@ import { CartContext } from "@/contexts/CartContext";
 import Image from "next/image";
 import Rating from "../Rating";
 
+const LOCAL_CURRENCY = process.env.NEXT_LOCAL_CURRENCY || "USD";
+
 const CartItemComponent = ({ cartItem }: { cartItem: CartItem }) => {
   const {
-    product: { title, image, description, category, rating },
+    product: { title, image, description, category, rating, price },
   } = cartItem;
+
+  const totalPrice = cartItem.quantity * price;
   return (
     <div className={styles.cartItem}>
       <div className={styles.cartItem__imgContainer}>
@@ -31,6 +35,20 @@ const CartItemComponent = ({ cartItem }: { cartItem: CartItem }) => {
           <p className={`small ${styles.cartItem__description}`}>
             {description}
           </p>
+          <div className={styles.cartItem__prices}>
+            <div className={styles.cartItem__priceGroup}>
+              <label className={styles.cartItem__priceLabel}>Each</label>
+              <p className={`subtitle ${styles.cartItem__price}`}>
+                {LOCAL_CURRENCY} {price}
+              </p>
+            </div>
+            <div className={styles.cartItem__priceGroup}>
+              <label className={styles.cartItem__priceLabel}>Total</label>
+              <h3 className={styles.cartItem__price}>
+                {LOCAL_CURRENCY} {totalPrice}
+              </h3>
+            </div>
+          </div>
         </div>
         <div>Actions</div>
       </div>
