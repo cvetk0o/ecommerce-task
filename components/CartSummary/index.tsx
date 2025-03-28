25b07e2363a13b09d4fd29754cb92fccf70b3352
cvetk0o <petar.ponjevic@gmail.com>
2025-03-28 19:12:37 +0100

    feature[component]: order summary details

--- components/CartSummary/index.tsx
+++ components/CartSummary/index.tsx
@@ -8,6 +8,7 @@ import Image from "next/image";
 import Rating from "../Rating";
 import DeleteProductButton from "../DeleteProductButton";
 import QuantityControl from "../QuantityControl";
+import Button from "../Button";
 
 const LOCAL_CURRENCY = process.env.NEXT_LOCAL_CURRENCY || "USD";
 
@@ -74,10 +75,10 @@ const CartItemComponent = ({ cartItem }: { cartItem: CartItem }) => {
 const CartSummary: React.FC = () => {
   const { cart } = useContext(CartContext) as CartContextType;
 
+  if (!cart || !cart.numberOfItems) {
+    return <h3>Empty Cart</h3>;
+  }
   const renderCartItems = () => {
-    if (!cart || !cart.numberOfItems) {
-      return <h3>Empty Cart</h3>;
-    }
     return cart.items.map((cartItem, index) => {
       return (
         <div className={styles.cartItem__container} key={cartItem.id}>
@@ -90,7 +91,44 @@ const CartSummary: React.FC = () => {
   return (
     <div className={styles.cartSummary}>
       <div className={styles.cartSummary__products}>{renderCartItems()}</div>
-      <div className={styles.cartSummary__summary}>Summary</div>
+      <div className={styles.cartSummary__summary}>
+        <h3>Order Summary</h3>
+        <div>
+          <div className={styles.summaryData}>
+            <p className="small">SubTotal</p>
+            <p className="small boldText">
+              {LOCAL_CURRENCY} {cart.totalPrice}
+            </p>
+          </div>
+          <div className={styles.summaryData}>
+            <p className="small">
+              Discount{" "}
+              {cart.discount?.percentage
+                ? `(-${cart.discount.percentage}%)`
+                : ""}
+            </p>
+            <p className="small boldText redText">
+              {" "}
+              - {LOCAL_CURRENCY} {cart.discount.amount}
+            </p>
+          </div>
+          <div className={styles.summaryData}>
+            <p className="small">Delivery Fee</p>
+            <p className="small boldText">
+              {LOCAL_CURRENCY} {cart.deliveryFee}
+            </p>
+          </div>
+        </div>
+
+        <div className="divider__thiner" />
+        <div className={styles.summaryData}>
+          <p>Total</p>
+          <p className="boldText">
+            {LOCAL_CURRENCY} {cart.totalPrice}
+          </p>
+        </div>
+        <Button>Go to Checkout</Button>
+      </div>
     </div>
   );
 };
