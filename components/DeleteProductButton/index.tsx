1e18e6ee942d8fc0e1dfca6470183e2fae9526d5
cvetk0o <petar.ponjevic@gmail.com>
2025-03-30 23:06:32 +0200

    feature: changed return types; toast error message

--- components/DeleteProductButton/index.tsx
+++ components/DeleteProductButton/index.tsx
@@ -3,6 +3,7 @@ import TrashIcon from "../../public/trashcan.svg";
 import Image from "next/image";
 import { useCartContext } from "@/contexts/CartContext";
 import { CartItem } from "@/types";
+import { useToast } from "@/contexts/ToastContext";
 
 interface IDeleteProductButtonProps {
   cartItem: CartItem;
@@ -12,13 +13,17 @@ const DeleteProductButton: React.FC<IDeleteProductButtonProps> = ({
   cartItem,
 }) => {
   const { removeCartItem } = useCartContext();
+  const { showToast } = useToast();
+
+  const handleRemoveCartItem = async () => {
+    const [error] = await removeCartItem(cartItem.id);
+
+    if (error) {
+      showToast("Failed to remove cart item:", "error");
+    }
+  };
   return (
-    <div
-      className="cursorPointer"
-      onClick={() => {
-        removeCartItem(cartItem.id);
-      }}
-    >
+    <div className="cursorPointer" onClick={handleRemoveCartItem}>
       <Image alt="cart" src={TrashIcon} width={24} height={24} />
     </div>
   );
