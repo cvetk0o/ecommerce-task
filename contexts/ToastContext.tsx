7843b061e40cc2e33a7f92b7079e71dd1e229c4c
cvetk0o <petar.ponjevic@gmail.com>
2025-03-30 21:52:04 +0200

    feature: toast messages

---
+++ contexts/ToastContext.tsx
@@ -0,0 +1,47 @@
+"use client";
+import { ToastContainer } from "@/components/Toast";
+import { createContext, useContext, useState, ReactNode } from "react";
+import { ToastType } from "@/types";
+
+interface ToastData {
+  id: string;
+  message: string;
+  type: ToastType;
+}
+
+interface ToastContextType {
+  showToast: (message: string, type: ToastType) => void;
+  removeToast: (id: string) => void;
+}
+
+const ToastContext = createContext<ToastContextType | undefined>(undefined);
+
+export function ToastProvider({ children }: { children: ReactNode }) {
+  const [toasts, setToasts] = useState<ToastData[]>([]);
+
+  const showToast = (message: string, type: ToastType) => {
+    const id = Math.random().toString(36).substr(2, 9); // Generate unique ID
+    setToasts((currentToasts) => [...currentToasts, { id, message, type }]);
+  };
+
+  const removeToast = (id: string) => {
+    setToasts((currentToasts) =>
+      currentToasts.filter((toast) => toast.id !== id)
+    );
+  };
+
+  return (
+    <ToastContext.Provider value={{ showToast, removeToast }}>
+      {children}
+      <ToastContainer toasts={toasts} onRemove={removeToast} />
+    </ToastContext.Provider>
+  );
+}
+
+export const useToast = () => {
+  const context = useContext(ToastContext);
+  if (context === undefined) {
+    throw new Error("useToast must be used within a ToastProvider");
+  }
+  return context;
+};
